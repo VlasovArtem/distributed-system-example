17bff5ac387abd7e744e0c4ecf781d39f32868f0
VlasovArtem <vlasovartem21@gmail.com>
2021-05-19 16:15:32 +0300

    Update grpc for message queue task

--- grpc/books/books_grpc.pb.go
+++ grpc/books/books_grpc.pb.go
@@ -21,6 +21,7 @@ const _ = grpc.SupportPackageIsVersion7
 type BooksClient interface {
 	GetBooks(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*BooksResponse, error)
 	FindBook(ctx context.Context, in *FindBookRequest, opts ...grpc.CallOption) (*Book, error)
+	AddBookAndAuthor(ctx context.Context, in *BookAndAuthor, opts ...grpc.CallOption) (*emptypb.Empty, error)
 }
 
 type booksClient struct {
@@ -49,12 +50,22 @@ func (c *booksClient) FindBook(ctx context.Context, in *FindBookRequest, opts ..
 	return out, nil
 }
 
+func (c *booksClient) AddBookAndAuthor(ctx context.Context, in *BookAndAuthor, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, "/Books/AddBookAndAuthor", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // BooksServer is the server API for Books service.
 // All implementations must embed UnimplementedBooksServer
 // for forward compatibility
 type BooksServer interface {
 	GetBooks(context.Context, *emptypb.Empty) (*BooksResponse, error)
 	FindBook(context.Context, *FindBookRequest) (*Book, error)
+	AddBookAndAuthor(context.Context, *BookAndAuthor) (*emptypb.Empty, error)
 	mustEmbedUnimplementedBooksServer()
 }
 
@@ -67,6 +78,9 @@ func (UnimplementedBooksServer) GetBooks(context.Context, *emptypb.Empty) (*Book
 }
 func (UnimplementedBooksServer) FindBook(context.Context, *FindBookRequest) (*Book, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method FindBook not implemented")
+}
+func (UnimplementedBooksServer) AddBookAndAuthor(context.Context, *BookAndAuthor) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AddBookAndAuthor not implemented")
 }
 func (UnimplementedBooksServer) mustEmbedUnimplementedBooksServer() {}
 
@@ -117,6 +131,24 @@ func _Books_FindBook_Handler(srv interface{}, ctx context.Context, dec func(inte
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Books_AddBookAndAuthor_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(BookAndAuthor)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(BooksServer).AddBookAndAuthor(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/Books/AddBookAndAuthor",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(BooksServer).AddBookAndAuthor(ctx, req.(*BookAndAuthor))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Books_ServiceDesc is the grpc.ServiceDesc for Books service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -132,7 +164,11 @@ var Books_ServiceDesc = grpc.ServiceDesc{
 			MethodName: "FindBook",
 			Handler:    _Books_FindBook_Handler,
 		},
+		{
+			MethodName: "AddBookAndAuthor",
+			Handler:    _Books_AddBookAndAuthor_Handler,
+		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "books/books.proto",
+	Metadata: "grpc/books/books.proto",
 }
